c4a69c668aa898412caf1685238bc16c2d161eeb
studioLuda <studio.luda.work@gmail.com>
2021-07-04 01:15:08 +0900

    상세보기 페이지 구현2 : dummy데이터와 상세보기 페이지 정보 동기화. ImgArea 사용하지 않는 a 태그 제거

--- src/components/ShopContainer.jsx
+++ src/components/ShopContainer.jsx
@@ -7,12 +7,12 @@ import { get } from '../util/commonUtils';
 
 export default function ShopContainer() {
   // todo : 여기서 fetch하여 데이터 가져옴
-  const { items } = shopItems;
+  const { dummyItems: items } = shopItems;
 
   // sortOption 값 확인 & 선택된 값대로 정렬후 새 items 리턴
   const itemSorts = {
     orderByCreatedAt: (a, b) => a.id - b.id,
-    orderByPopularity: (a, b) => b.score - a.score,
+    orderByPopularity: (a, b) => b.averageScore - a.averageScore,
     orderByLowPrice: (a, b) => a.realPrice - b.realPrice,
     orderByHighPrice: (a, b) => b.realPrice - a.realPrice,
   };
--- src/pages/ShopItemDetailPage.test.jsx
+++ src/pages/ShopItemDetailPage.test.jsx
@@ -5,12 +5,15 @@ import ShopItemDetailPage from './ShopItemDetailPage';
 
 describe('ShopItemDetailPage', () => {
   it('renders page', () => {
+    const params = { id: 1 };
     const { container } = render(
       <MemoryRouter>
-        <ShopItemDetailPage />
+        <ShopItemDetailPage params={params} />
       </MemoryRouter>,
     );
 
     expect(container).toHaveTextContent('상세보기');
+    expect(container).toHaveTextContent('수량');
+    expect(container).toHaveTextContent('장바구니 담기 ');
   });
 });
