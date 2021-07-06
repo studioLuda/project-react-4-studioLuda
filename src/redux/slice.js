431b4cc21d8529a16a1681aa538ae83df7d6894e
studioLuda <studio.luda.work@gmail.com>
2021-07-06 20:28:56 +0900

    CartPage > checkBox 기능 구현,  테스트 >추가
    1) 체크박스를 클릭하면 변경된 값이 반영>됩니다.
    
    2) 선택상품 삭제하기 버튼을 누르면, 체크
    된 아이템이 삭제됩니다.
    
    3) 테스트가 추가되었습니다.

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -24,7 +24,50 @@ const reducers = {
       itemAmount,
     };
   },
+  synchonizeCart(state) {
+    const cart = loadObjItem('cart') || [];
+    return {
+      ...state,
+      cart,
+    };
+  },
+  changeCartItemCheked(state, { payload: { itemId, checked, cart } }) {
+    console.log('slice확인:', cart); // 현재 컴포넌트에 업데이트된 cart 값 (로컬스토리지에서 불러옴)
+    console.log('state확인:', state.cart); // -> 없음 cart가 store 업데이트 되지않음.
+    // FIXME! 일단 커밋하고, 리팩토링 하기 => 부모 컴포넌트에서 cart값 불러오고,
+    // 자식 컴포넌트에서 변경하는 로직 구현해볼 예정.
+    // 왜 이런 현상이 생기는지 잘 모르겠음. 질문하기.
 
+    console.log('itemId', itemId);
+    console.log('cheked', checked);
+
+    const index = cart.findIndex((item) => item.id === Number(itemId));
+    if (index > -1) {
+      const updatedItem = {
+        ...cart[index],
+        checked,
+      };
+      const updatedCart = [...cart];
+      updatedCart[index] = updatedItem;
+
+      return {
+        ...state,
+        cart: updatedCart,
+      };
+    }
+    return state;
+  },
+  removeSelectedCartIem(state, { payload: cart }) {
+    console.log('slice확인:', cart);
+    console.log('state확인:', state.cart);
+
+    const updatedCart = [...cart].filter((e) => !e.checked);
+    saveObjItem('cart', updatedCart);
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  },
   addItemToCart(state, { payload: shopItem }) {
     const { id } = shopItem;
     const { itemAmount } = state;
@@ -57,7 +100,6 @@ const reducers = {
     // todo : 중복제거 리팩토링 할 것.
     // todo : 로그인 할 경우, user의 장바구니를 서버에 저장할 것인지 고민
   },
-
 };
 
 const { actions, reducer } = createSlice({
@@ -71,4 +113,7 @@ export const {
   selectSortOption,
   changeItemAmountField,
   addItemToCart,
+  synchonizeCart,
+  changeCartItemCheked,
+  removeSelectedCartIem,
 } = actions;
